91c8c9d1b92250f3c56b096ce00c41c6922078cf
Brian Bixby <brianbixby0@gmail.com>
2018-01-17 11:10:59 -0800

    done except read me and stretch goal

--- christian-brian/scripts/article.js
+++ christian-brian/scripts/article.js
@@ -7,25 +7,23 @@ function Article (rawDataObj) {
   this.category = rawDataObj.category;
   this.body = rawDataObj.body;
   this.publishedOn = rawDataObj.publishedOn;
-  // console.log(this);
 }
 
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 // COMMENT: Why isn't this method written as an arrow function?
-//
+// on line 27 we are resetting the value of this.body to marked(this.body)
+
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
-  // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // this is a ternary statement that says if there's a publishedOn property that equals the published status then published status = `published ${this.daysAgo} days ago`  or else it sets it equal to a draft
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
-  // console.log(this);
   return template(this);
 };
 
@@ -34,7 +32,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-
+// loadall() is called from fetchAll() and rawData is the json array that we pull from localStorage or the api
 
 Article.loadAll = rawData => {
   rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
@@ -47,19 +45,18 @@ Article.loadAll = rawData => {
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
-    console.log(JSON.parse(localStorage.rawData));
+    // console.log(JSON.parse(localStorage.rawData));
     Article.loadAll(JSON.parse(localStorage.rawData));
   } else {
     console.log(localStorage);
     $.getJSON('/data/hackerIpsum.json')
       .then(data => {
         localStorage.setItem('rawData', JSON.stringify(data));
-        console.log(localStorage.rawData);
+        // console.log(localStorage.rawData);
         Article.loadAll(data);
       });
   }
 }
 
-// $(document).ready(function() {
-//   Article.fetchAll();
-// });
+// We determined the order of execution by putting a console.log into every function from here we noticed that a couple of functions were not being executed and we had to call these functions, we ended up refactoring the code and putting the fetchall in the index.html and from here we loadall() and then call initIndexPage function.
+
